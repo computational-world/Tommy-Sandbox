252f060ce21e181c11d7b5129a563c50f60b9a11
ArimosTX <47014845+ArimosTX@users.noreply.github.com>
2019-01-31 16:15:17 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -116,11 +116,66 @@ Platform.prototype.update = function () {
 }
 
 /*
+Blue Monster
+*/
+function BlueMonster(game, spritesheet, x, y, width, height) {
+//	this.animation = new Animation(spritesheet, 25, 25, 8, 0.10, 10, true, 1);
+    this.ctx = game.ctx;
+    this.spritesheet = spritesheet;
+    this.game = game;
+	this.width = width;
+    this.height = height;
+	this.speed = 50;
+    this.x = x;
+    this.y = y;
+	this.active = false;
+    this.boundingbox = new BoundingBox(x, y, width, height);
+    Entity.call(this, game, x, y, width, height);
+}
+
+BlueMonster.prototype = new Entity();
+BlueMonster.prototype.constructor = BlueMonster;
+
+BlueMonster.prototype.draw = function () {
+	this.lastLeft = this.boundingbox.left + 5;
+	if (this.game.Hero.x > 128 * 12) {
+		this.active = true;
+	}
+	if (this.active) {
+		this.x -= this.game.clockTick * this.speed;
+	}
+    this.ctx.drawImage(this.spritesheet, this.x - Camera.x, this.y);
+	
+	// check for bullet
+	for (var i = 0; i < this.game.bullets.length; i++) {
+		var bullet = this.game.bullets[i];
+		
+		// hit by bullet            
+		if (this.boundingbox.collide(bullet.boundingbox) && this.lastLeft > bullet.boundingbox.right) {
+			console.log("hit!");
+		}
+	}
+    
+	this.ctx.strokeStyle = "red";
+	this.ctx.strokeRect(this.x, this.y, this.width, this.height);
+//	this.ctx.strokeStyle = "green";
+//	this.ctx.strokeRect(this.boundingbox.x, this.boundingbox.y, this.boundingbox.width, this.boundingbox.height);
+	
+    Entity.prototype.draw.call(this);
+}
+BlueMonster.prototype.update = function () {
+    Entity.prototype.update.call(this);
+}
+
+/*
 Running Soldier
 */
 function Soldier(game, spritesheet, x, y) {
-    this.animation = new Animation(spritesheet, 50, 50, 8, 0.10, 8, true, 1);
-    this.speed = 400;
+    this.animationRight = new Animation(spritesheet, 50, 50, 8, 0.10, 8, true, 1);
+	this.animationLeft = new Animation(AM.getAsset("./img/soldierLeft.png"), 50, 50, 8, 0.10, 8, true, 1);
+    this.animationJumpRight = new Animation(AM.getAsset("./img/soldierJumpRight.png"), 50, 50, 8, 0.10, 8, true, 1);
+	this.animationJumpLeft = new Animation(AM.getAsset("./img/soldierJumpLeft.png"), 50, 50, 8, 0.10, 8, true, 1);
+	this.speed = 400;
     this.ctx = game.ctx;
     this.game = game;
     this.x = x;
@@ -136,6 +191,7 @@ function Soldier(game, spritesheet, x, y) {
     this.platform = game.platforms[0];
     
     this.boundingbox = new BoundingBox(this.x+2, this.y+2, this.width-7, this.height-3);
+	
     Entity.call(this, game, x, y);
 }
 
@@ -144,25 +200,39 @@ Soldier.prototype.constructor = Soldier;
 
 Soldier.prototype.update = function () {
 
+	this.lastboundingbox = new BoundingBox(this.x+2, this.y+2, this.width-7, this.height-13);
     // moving
     if (this.moving) {
-        this.x += this.game.clockTick * this.speed * this.direction;    
+		this.lastRight = this.boundingbox.right-3;
+        
+		this.x += this.game.clockTick * this.speed * this.direction;    
         Camera.x += this.game.clockTick * this.speed * this.direction;      
-    }
+/*    
+		// check for platform
+        for (var i = 0; i < this.game.platforms.length; i++) {
+            var pf = this.game.platforms[i];
+            
+            // landed on top of platform
+            if (this.boundingbox.collide(pf.boundingbox) && this.lastRight < pf.boundingbox.left && this.lastboundingbox.bottom < pf.boundingbox.bottom) {
+                console.log("test");
+                this.x = pf.boundingbox.left - this.width;
+            }
+        }
+*/
+		}
 
     // free fall
     if (this.falling) {
-        this.lastBottom = this.boundingbox.bottom-3;
         this.boundingbox = new BoundingBox(this.x+2, this.y+2, this.width-7, this.height-3);
     
-        this.y += this.game.clockTick / this.animation.totalTime * 4 * this.jumpHeight;
+        this.y += this.game.clockTick / this.animationRight.totalTime * 4 * this.jumpHeight;
         
         // check for platform
         for (var i = 0; i < this.game.platforms.length; i++) {
             var pf = this.game.platforms[i];
             
             // landed on top of platform
-            if (this.boundingbox.collide(pf.boundingbox) && this.lastBottom < pf.boundingbox.top) {
+            if (this.boundingbox.collide(pf.boundingbox) && this.lastboundingbox.bottom < pf.boundingbox.top) {
                 console.log("Collision!");
                 this.falling = false;
                 this.y = pf.boundingbox.top - this.height+3;
@@ -179,10 +249,17 @@ Soldier.prototype.update = function () {
         
     }
     if (this.jumping) {
-        var duration = this.animation.elapsedTime + this.game.clockTick;
-        if (duration > this.animation.totalTime / 2) duration = this.animation.totalTime - duration;
-            duration = duration / this.animation.totalTime;
-            
+		var duration;
+        if (this.direction === 1) {
+			duration = this.animationJumpRight.elapsedTime + this.game.clockTick;
+			if (duration > this.animationJumpRight.totalTime / 2) duration = this.animationJumpRight.totalTime - duration;
+			duration = duration / this.animationJumpRight.totalTime;
+		} else {
+			duration = this.animationJumpLeft.elapsedTime + this.game.clockTick;
+			if (duration > this.animationJumpLeft.totalTime / 2) duration = this.animationJumpLeft.totalTime - duration;
+			duration = duration / this.animationJumpLeft.totalTime;	
+		}
+		
         // parbolic jump
         height = (4 * duration - 4 * duration * duration) * this.jumpHeight;
         this.y = this.base - height;
@@ -219,26 +296,76 @@ Soldier.prototype.update = function () {
 
     
     if (this.y > 700) this.y = -50;
-    this.moving = false;
+
     Entity.prototype.update.call(this);
 }
 
 Soldier.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x - Camera.x, this.y);
-    //this.ctx.rect(this.x+2, this.y+2, this.width-7, this.height-3);
+	if (this.moving && !this.falling) {
+		
+		if (this.direction === 1) {
+			this.animationRight.drawFrame(this.game.clockTick, this.ctx, this.x - Camera.x, this.y);
+			if (this.animationRight.isDone()) {
+				this.animationRight.elapsedTime = 0;
+				this.moving = false;
+			}
+				
+		}
+		else {
+			this.animationLeft.drawFrame(this.game.clockTick, this.ctx, this.x - Camera.x, this.y);
+			if (this.animationLeft.isDone()) {
+				this.animationLeft.elapsedTime = 0;
+				this.moving = false;
+			}
+		}
+	}
+	else if (this.jumping) {
+		if (this.direction === 1) {
+			this.animationJumpRight.drawFrame(this.game.clockTick, this.ctx, this.x - Camera.x, this.y);
+			if (this.animationJumpRight.isDone()) {
+				this.animationJumpRight.elapsedTime = 0;
+				this.jumping = false;
+				this.falling = true;
+			}	
+		}
+		else {
+			this.animationJumpLeft.drawFrame(this.game.clockTick, this.ctx, this.x - Camera.x, this.y);
+			if (this.animationJumpLeft.isDone()) {
+				this.animationJumpLeft.elapsedTime = 0;
+				this.jumping = false;
+				this.falling = true;
+			}
+		}
+		
+	}
+	else if (this.falling) {
+		if (this.direction === 1) this.ctx.drawImage(AM.getAsset("./img/soldier.png"), 311, 11, 48, 49, this.x - Camera.x, this.y, 50, 50);
+		else 
+			this.ctx.drawImage(AM.getAsset("./img/soldier.png"), 361, 11, 49, 49, this.x - Camera.x, this.y, 50, 50);
+	}
+	else if (!this.moving) {
+		if (this.direction === 1) this.ctx.drawImage(AM.getAsset("./img/soldier.png"), 8, 11, 49, 49, this.x - Camera.x, this.y, 50, 50);
+		else 
+			this.ctx.drawImage(AM.getAsset("./img/soldier.png"), 60, 11, 47, 49, this.x - Camera.x, this.y, 50, 50);
+	}
+	
+	//this.ctx.rect(this.x+2, this.y+2, this.width-7, this.height-3);
     //this.ctx.stroke();
+	//this.moving = false;
     Entity.prototype.draw.call(this);
 }
 
 /*
 Bullet
 */
-function Bullet(game, spritesheet, x, y) {
+function Bullet(game, spritesheet, x, y, direction) {
     this.animation = new Animation(spritesheet, 14, 14, 8, 1, 8, true, 1);
     this.speed = 500;
     this.ctx = game.ctx;
     this.x = x;
     this.y = y;
+	this.direction = direction;
+	this.boundingbox = new BoundingBox(this.x, this.y, this.width, this.height);
     Entity.call(this, game, this.x, this.y);
 }
 
@@ -246,7 +373,7 @@ Bullet.prototype = new Entity();
 Bullet.prototype.constructor = Bullet;
 
 Bullet.prototype.update = function () {
-    this.x += this.game.clockTick * this.speed;
+    this.x += this.game.clockTick * this.speed * this.direction;
     /*
     if (this.x > Camera.x + 800) {
         this.removeFromWorld();
@@ -267,11 +394,16 @@ var Camera = {
 };
 
 AM.queueDownload("./img/soldier.png");
+AM.queueDownload("./img/soldierLeft.png");
+AM.queueDownload("./img/soldierRight.png");
+AM.queueDownload("./img/soldierJumpRight.png");
+AM.queueDownload("./img/soldierJumpLeft.png");
 AM.queueDownload("./img/layer1.png");
 AM.queueDownload("./img/layer2.png");
 AM.queueDownload("./img/layer3.png");
 AM.queueDownload("./img/platform.png");
 AM.queueDownload("./img/bullet.png");
+AM.queueDownload("./img/BlueMonster.png");
 
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
@@ -280,6 +412,9 @@ AM.downloadAll(function () {
 	var gameEngine = new GameEngine();
     var platforms = [];
     var bullets = [];
+	gameEngine.bullets = bullets;
+	var monsters = [];
+	gameEngine.monsters = monsters;
     gameEngine.init(ctx);
     gameEngine.start();
 
@@ -307,19 +442,41 @@ AM.downloadAll(function () {
     var pf = new Platform(gameEngine, AM.getAsset("./img/platform.png"), (128*7), 300, 128, 128)
 		gameEngine.addEntity(pf);
         platforms.push(pf);
+		
+    for (var i = 0; i < platformwidth; i++) {
+        var pf = new Platform(gameEngine, AM.getAsset("./img/platform.png"), (128*10)+(128*i), 500, 128, 128)
+        gameEngine.addEntity(pf);
+        platforms.push(pf);
+    }
+	
+	for (var i = 0; i < platformwidth; i++) {
+        var pf = new Platform(gameEngine, AM.getAsset("./img/platform.png"), (128*11)+(128*i), 400, 128, 128)
+        gameEngine.addEntity(pf);
+        platforms.push(pf);
+    }
     
     gameEngine.platforms = platforms;
     
-    var Hero = new Soldier(gameEngine, AM.getAsset("./img/soldier.png"), 400, 0);
+    var Hero = new Soldier(gameEngine, AM.getAsset("./img/soldierRight.png"), 400, 0);
     gameEngine.addEntity(Hero);
 	gameEngine.Hero = Hero;
 
+	var monster1 = new BlueMonster(gameEngine, AM.getAsset("./img/BlueMonster.png"), 128*15, 375, 26, 27);
+	gameEngine.addEntity(monster1);
+	monsters.push(monster1);
+	
     document.addEventListener('keydown', function(e){
           
 		switch(e.keyCode) {
 			// Spacebar
 			case 32:
-				var bullet = new Bullet(gameEngine, AM.getAsset("./img/bullet.png"), Hero.x + 37, Hero.y + 18);
+			var compensate = 37;
+			var dir = 1;
+			if (Hero.direction === -1) {
+				dir = -1;
+				compensate = -10;
+			}
+				var bullet = new Bullet(gameEngine, AM.getAsset("./img/bullet.png"), Hero.x + compensate, Hero.y + 18, dir);
 				gameEngine.addEntity(bullet);
 				bullets.push(bullet);
 			break;
@@ -344,7 +501,19 @@ AM.downloadAll(function () {
       });
 	document.addEventListener('keyup', function(e){
 		switch(e.keyCode) {
-            case 38:
+			//left
+			case 37:
+			Hero.moving = false;
+			break;
+			
+			// right
+			case 39:
+			Hero.moving = false;
+			break;
+			
+			//up
+			case 38:
+			Hero.up = false;
 			break;
 		}
 	});
